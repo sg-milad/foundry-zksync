46bc42b97eaf7a6a24b36a64f9d5125a6cbed92e
elfedy <federico@moonsonglabs.com>
2025-01-16 15:41:53 -0300

    ee eee ee

--- crates/forge/tests/it/invariant.rs
+++ crates/forge/tests/it/invariant.rs
@@ -611,6 +611,8 @@ async fn test_invariant_roll_fork_handler() {
     let filter = Filter::new(".*", ".*", ".*fuzz/invariant/common/InvariantRollFork.t.sol");
     let mut runner = TEST_DATA_DEFAULT.runner_with(|config| {
         config.fuzz.seed = Some(U256::from(119u32));
+        config.invariant.runs = 1;
+        config.invariant.depth = 5;
     });
     let results = runner.test_collect(&filter);
     assert_multiple(
